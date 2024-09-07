20541bd9897ef438fafa37df7cec0c4c2d202b17
asdwerte1 <samjmarkham@outlook.com>
2024-09-07 18:32:24 +0100

    Calcualtor in functional state - updates to be made

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const calculator = {
         return num1 / num2;
     },
 
-    operate: function (num1, num2, operation) {
+    operate(num1, num2, operation) {
         switch (operation) {
             case "+": return this.add(num1, num2);
             case "-": return this.subtract(num1, num2);
-            case "*": return this.multiply(num1, num2);
-            case "/": return this.divide(num1, num2);
+            case "x": return this.multiply(num1, num2);
+            case "÷" : return this.divide(num1, num2);
         }
     }
 }
@@ -30,11 +30,11 @@ const calculator = {
 const callStack = {
     stack: [],
 
-    addToStack: function (item) {
+    addToStack(item) {
         this.stack.push(item);
     },
 
-    clearStack: function () {
+    clearStack() {
         this.stack.length = 0;
     }
 }
@@ -42,6 +42,7 @@ const callStack = {
 document.addEventListener("DOMContentLoaded", () => {
 
     const numbers = document.querySelectorAll(".number");
+    const operators = document.querySelectorAll(".operator");
     const text = document.getElementById("display-text");
     text.innerHTML = "";
     const delay = 300;
@@ -62,8 +63,6 @@ document.addEventListener("DOMContentLoaded", () => {
         text.innerHTML = "";
     }, welcomeMessage.length * delay + 1000);
 
-    console.log(numbers);
-
     for (const number of numbers) {
         number.addEventListener("click", () => {
 
@@ -73,8 +72,36 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     }
 
+    for (const operator of operators) {
+        operator.addEventListener("click", () => {
+
+            if (callStack.stack.length === 0 && text.innerHTML.length !== 0) { // Ensure call stack is empty but there is a number to operate on
+                const numberToAdd = Number(text.innerHTML);
+                callStack.addToStack(numberToAdd);
+                callStack.addToStack(operator.textContent.trim());
+                text.innerHTML = "";
+            } // Add else to perform action if stack has contnet - this will cause an operation to run and show result in the screen
+              // I.e. if a user presses add, then another number, then times, this will perform the add in the call stack
+
+        })
+    }
+
+    document.getElementById("equals").addEventListener("click", () => {
+        if (callStack.stack.length !== 0) {
+            const num1 = callStack.stack[0];
+            const operator = callStack.stack[1];
+            const num2 = Number(text.textContent.trim());
+            const result = calculator.operate(num1, num2, operator);
+            callStack.stack = [];
+            text.innerHTML = result;
+
+            // Implement something to either prevent people from typing numbers after answer - or to clear answer when they start typing
+        }
+    })
+
     document.getElementById("clear").addEventListener("click", () => {
         text.innerHTML = "";
+        callStack.stack = [];
     })
 });
 
